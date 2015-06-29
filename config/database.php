78cb25fb900ce5ebb6350efeee163ee746b2c327
Şəhriyar İmanov <shehi@imanov.me>
2015-06-29 12:47:23 +0300

    Switched Session handler from file to Redis and did the same for caching + Updated Elixir version.

--- config/database.php
+++ config/database.php
@@ -102,8 +102,8 @@ return [
     'redis' => [
         'cluster' => false,
         'default' => [
-            'host' => '127.0.0.1',
-            'port' => 6379,
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
     ],
