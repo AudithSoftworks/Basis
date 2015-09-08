0b2513606da7e9fab472c0b82d7fffbe69f49dda
Şəhriyar İmanov <shehi@imanov.me>
2015-09-08 15:00:07 +0300

    Cleaned HomeController a bit.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,26 +2,16 @@
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth', ['only' => ['getProfile']]);
-    }
-
     public function getIndex()
     {
         $name = trans('auth.guest');
         $userType = self::TRANSLATION_TAG_GUEST_USER;
 
-        if ($user = \Sentinel::getUser()) {
+        if ($user = app('sentinel')->getUser()) {
             $name = $user->name;
             $userType = self::TRANSLATION_TAG_REGISTERED_USER;
         }
 
         return view('index', ['userType' => $userType, 'name' => $name]);
-    }
-
-    public function getProfile()
-    {
-        return view('profile', ['userType' => self::TRANSLATION_TAG_REGISTERED_USER, 'name' => \Sentinel::getUser()->name]);
     }
 }
