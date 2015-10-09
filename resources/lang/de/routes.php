ab900545eccd703d78e908344f1c7c15f9dc91f0
Florian Chrometz <f@chrmtz.de>
2015-10-09 09:47:07 +0200

    added german translation

---
+++ resources/lang/de/routes.php
@@ -0,0 +1,42 @@
+<?php
+
+// Password Reminder Language Lines
+return [
+    '' => [
+        'locale' => 'de',
+        'resources' => [
+            'index' => 'Index',
+            'create' => 'erstellen',
+            'store' => 'speichern',
+            'show' => 'anzeigen',
+            'edit' => 'bearbeiten',
+            'update' => 'erneuern',
+            'destroy' => 'löschen'
+        ]
+    ],
+    'files' => [
+        '' => 'Dateien'
+    ],
+    'login' => [
+        '' => 'Anmelden',
+    ],
+    'logout' => [
+        '' => 'Abmelden'
+    ],
+    'register' => [
+        '' => 'Registrieren'
+    ],
+    'activation' => [
+        '' => 'Aktivierung',
+        'code' => 'Code',
+        'process' => 'Prozess'
+    ],
+    'password' => [
+        '' => 'Passwort',
+        'email' => 'E-Mail',
+        'reset' => 'zurücksetzen'
+    ],
+    'users' => [
+        '' => 'Benutzer'
+    ]
+];
