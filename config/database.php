cccbe64474ea838a6af7cfc09e48100071524753
Şəhriyar İmanov <shehi@imanov.me>
2015-10-28 16:36:20 +0300

    Minor modifications to Docker build scripts + Fixed Redis database config where environmental variable REDIS_PORT conflicted with Docker-created variable with same name.

--- config/database.php
+++ config/database.php
@@ -103,7 +103,7 @@ return [
         'cluster' => false,
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'port' => env('REDIS_PORT', 6379),
+            'port' => 6379,
             'database' => 0,
         ],
     ],
